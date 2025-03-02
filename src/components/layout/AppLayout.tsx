7e67df5ed4ebf18cf2eafaf893970913e956d233
Moses Nwigberi <moses.nwigberi@ditcosoft.com>
2025-03-02 23:58:35 +0100

    Added scroll indicator

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,12 +1,11 @@
-import React, { ReactNode } from "react";
+"use client";
+import React, { ReactNode, useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import dynamic from "next/dynamic";
 const Cursor = dynamic(() => import("../shared/CustomCusor"), {
   ssr: false,
 });
-
-// import CustomCursor from "../shared/CustomCusor";
 // import Loader from "../shared/Loader";
 
 type Props = {
@@ -14,15 +13,44 @@ type Props = {
 };
 
 const AppLayout = (props: Props) => {
+  // const [_, setTheme] = useState<"dark" | "light">("dark");
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const scrollHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const progress = (scrollY / scrollHeight) * 100;
+
+      setScrollProgress(progress);
+
+      // if (scrollY > 1000) {
+      //   setTheme("light");
+      // } else {
+      //   setTheme("dark");
+      // }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <>
+    <div>
+      {/* Scroll Progress Bar */}
+      <div
+        className="fixed z-50 top-0 left-0 h-1 bg-[#14AFF1]"
+        style={{ width: `${scrollProgress}%` }}
+      ></div>
+
       {/* <Loader /> */}
       <Header />
+
       {props.children}
       {/* <ScrollToTop /> */}
       <Cursor />
       <Footer />
-    </>
+    </div>
   );
 };
 
