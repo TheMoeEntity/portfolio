ee92d23b764872fcbcd5669365f4ffdd0fdb9f74
Moses Nwigberi <moses.nwigberi@ditcosoft.com>
2025-03-08 13:51:12 +0100

    Added Loader

--- src/components/sections/Home/Hero.tsx
+++ src/components/sections/Home/Hero.tsx
@@ -5,6 +5,8 @@ import { Brands } from "../Brands";
 import years from "../../../app/images/years.png";
 import Image from "next/image";
 import { useTheme } from "@/context/ThemeContext";
+import Loader from "@/components/shared/Loader";
+import { useState } from "react";
 const Hero = () => {
   const services = [
     "Frontend Development",
@@ -44,56 +46,47 @@ const Hero = () => {
       transition: { type: "spring", stiffness: 100, damping: 12 },
     },
   };
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoadingComplete = () => {
+    setIsLoading(false); // Update loading state when loader finishes
+  };
   const { theme } = useTheme();
   return (
     <section className="container lg:mt-10 mx-auto lg:px-4 lg:py-4">
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={containerVariants}
-        className="flex flex-col w-full mb-2 text-left  "
-      >
-        <motion.h1
-          variants={containerVariants}
-          className={`mb-2 text-6xl font-bold tracking-tighter ${theme === "light" ? "text-[#110f10]" : "text-white"} lg:text-8xl md:text-7xl`}
-        >
-          {"Crafting Digital Experiences, One Arrow Function at a Time"
-            .split(" ")
-            .map((word, index) => (
-              <motion.span
-                key={index}
-                className="inline-block mr-2"
-                variants={childVariants}
-              >
-                {word}
-              </motion.span>
-            ))}
-        </motion.h1>
-        <br></br>
-
-        <motion.p
-          className="font-normal leading-relaxed text-gray-500 dark:text-gray-300 lg:w-2/3"
-          variants={containerVariants}
+      {isLoading && <Loader onLoadingComplete={handleLoadingComplete} />}
+      {!isLoading && (
+        <motion.div
           initial="hidden"
           animate="visible"
+          variants={containerVariants}
+          className="flex flex-col w-full mb-2 text-left  "
         >
-          {"I’m a ".split(" ").map((word, index) => (
-            <motion.span
-              key={index}
-              className="inline-block mr-1"
-              variants={childVariants}
-            >
-              {word}
-            </motion.span>
-          ))}
-          <Typewriter
-            repeatOnView={false}
-            text="frontend-heavy full-stack developer"
-            delay={50}
-          />
-          {" based in Lagos, Nigeria, working with clients and teams from all over the world. I specialize in building "
-            .split(" ")
-            .map((word, index) => (
+          <motion.h1
+            variants={containerVariants}
+            className={`mb-2 text-6xl font-bold tracking-tighter ${theme === "light" ? "text-[#110f10]" : "text-white"} lg:text-8xl md:text-7xl`}
+          >
+            {"Crafting Digital Experiences, One Arrow Function at a Time"
+              .split(" ")
+              .map((word, index) => (
+                <motion.span
+                  key={index}
+                  className="inline-block mr-2"
+                  variants={childVariants}
+                >
+                  {word}
+                </motion.span>
+              ))}
+          </motion.h1>
+          <br></br>
+
+          <motion.p
+            className="font-normal leading-relaxed text-gray-500 dark:text-gray-300 lg:w-2/3"
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+          >
+            {"I’m a ".split(" ").map((word, index) => (
               <motion.span
                 key={index}
                 className="inline-block mr-1"
@@ -102,14 +95,44 @@ const Hero = () => {
                 {word}
               </motion.span>
             ))}
-          <Typewriter
-            repeatOnView={false}
-            text="beautiful, responsive, and high-performance web applications"
-            delay={50}
-          />
-          {" that deliver seamless user experiences. With a strong focus on "
-            .split(" ")
-            .map((word, index) => (
+            <Typewriter
+              repeatOnView={false}
+              text="frontend-heavy full-stack developer"
+              delay={50}
+            />
+            {" based in Lagos, Nigeria, working with clients and teams from all over the world. I specialize in building "
+              .split(" ")
+              .map((word, index) => (
+                <motion.span
+                  key={index}
+                  className="inline-block mr-1"
+                  variants={childVariants}
+                >
+                  {word}
+                </motion.span>
+              ))}
+            <Typewriter
+              repeatOnView={false}
+              text="beautiful, responsive, and high-performance web applications"
+              delay={50}
+            />
+            {" that deliver seamless user experiences. With a strong focus on "
+              .split(" ")
+              .map((word, index) => (
+                <motion.span
+                  key={index}
+                  className="inline-block mr-1"
+                  variants={childVariants}
+                >
+                  {word}
+                </motion.span>
+              ))}
+            <Typewriter
+              text="frontend technologies"
+              repeatOnView={false}
+              delay={50}
+            />
+            {" and a solid foundation in ".split(" ").map((word, index) => (
               <motion.span
                 key={index}
                 className="inline-block mr-1"
@@ -118,28 +141,28 @@ const Hero = () => {
                 {word}
               </motion.span>
             ))}
-          <Typewriter
-            text="frontend technologies"
-            repeatOnView={false}
-            delay={50}
-          />
-          {" and a solid foundation in ".split(" ").map((word, index) => (
-            <motion.span
-              key={index}
-              className="inline-block mr-1"
-              variants={childVariants}
-            >
-              {word}
-            </motion.span>
-          ))}
-          <Typewriter
-            repeatOnView={false}
-            text="backend development"
-            delay={50}
-          />
-          {", I bring ideas to life by combining "
-            .split(" ")
-            .map((word, index) => (
+            <Typewriter
+              repeatOnView={false}
+              text="backend development"
+              delay={50}
+            />
+            {", I bring ideas to life by combining "
+              .split(" ")
+              .map((word, index) => (
+                <motion.span
+                  key={index}
+                  className="inline-block mr-1"
+                  variants={childVariants}
+                >
+                  {word}
+                </motion.span>
+              ))}
+            <Typewriter
+              repeatOnView={false}
+              text="clean code, modern tools, and creative problem-solving"
+              delay={50}
+            />
+            {".".split(" ").map((word, index) => (
               <motion.span
                 key={index}
                 className="inline-block mr-1"
@@ -148,22 +171,9 @@ const Hero = () => {
                 {word}
               </motion.span>
             ))}
-          <Typewriter
-            repeatOnView={false}
-            text="clean code, modern tools, and creative problem-solving"
-            delay={50}
-          />
-          {".".split(" ").map((word, index) => (
-            <motion.span
-              key={index}
-              className="inline-block mr-1"
-              variants={childVariants}
-            >
-              {word}
-            </motion.span>
-          ))}
-        </motion.p>
-      </motion.div>
+          </motion.p>
+        </motion.div>
+      )}
       <div className="w-full mt-10 flex text-lg flex-col md:flex-row md:justify-between">
         <article className="flex flex-1 flex-col gap-10">
           <p className=" font-normal">
