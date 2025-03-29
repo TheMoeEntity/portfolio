dd4aa4978dbca977127f948d8fe36d6ef3df60bb
Moses Nwigberi <moses.nwigberi@ditcosoft.com>
2025-03-29 10:44:40 +0100

    `Update email address in FloatingHeader component`

--- src/components/shared/UI/FloatingHeader.tsx
+++ src/components/shared/UI/FloatingHeader.tsx
@@ -35,7 +35,7 @@ const FloatingHeader = () => {
         </div>
       </div>
       <a
-        href="mailto:hello@roxanehassler.com"
+        href="mailto:mosesnwigberi@gmail.com"
         rel="noopener"
         className="mt-2 inline-block whitespace-nowrap duration-500 bg-[#1D1D1D] text-white px-4 py-2 transition-colors rounded-md text-center hover:bg-[#14AFF1]"
       >
