fd4760a65e29139f47c248baa65ca5c50f50c6e8
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2025-11-17 05:10:21 +0100

    Update FloatingHeader.tsx
    Updated CTA mail

--- src/components/shared/UI/FloatingHeader.tsx
+++ src/components/shared/UI/FloatingHeader.tsx
@@ -35,7 +35,7 @@ const FloatingHeader = () => {
         </div>
       </div>
       <a
-        href="mailto:mosesnwigberi@gmail.com"
+        href="mailto:contact@mosesnwigberi.com"
         rel="noopener"
         className="mt-2 inline-block whitespace-nowrap duration-500 bg-[#1D1D1D] text-white px-4 py-2 transition-colors rounded-md text-center hover:bg-[#14AFF1]"
       >
