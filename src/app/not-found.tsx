5804afa3e6c8b474a012cca01221e5bfa8501d5b
Moses Nwigberi <mosesnwigberi@gmail.com>
2025-11-19 15:31:17 +0100

    updated image position

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -29,7 +29,7 @@ const NotFound = () => {
   const imageStyles: CSSProperties = {
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-    backgroundPosition: "bottom left",
+    backgroundPosition: "top",
     backgroundImage: `url('/profile.png')`,
   };
 
